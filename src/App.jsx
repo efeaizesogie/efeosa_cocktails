3acaf3dd8cac49e9f88113ec6ab9295de0e81ca7
efeaizesogie <efeaizesogie@gmail.com>
2025-06-24 22:22:03 +0100

    create Hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { ScrollTrigger} from "gsap/all"
 import gsap from "gsap"
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
  gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
     return (
-        <div>
-            <h1 className="text-5xl text-green-200">Hello Gsap</h1>
-        </div>
+        <main>
+            <Navbar/>
+            <Hero/>
+            <div className="h-screen"></div>
+        </main>
     )
 }
 export default App
